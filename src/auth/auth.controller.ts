38192e6bf4f9c7224958ae810935db5a8276d290
bleek42 <brandonleek42@gmail.com>
2021-10-15 21:43:13 -0400

    added auth module, controller, service, register dto, post endpoint

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from '@nestjs/common';
+
+import { RegisterDto } from './dtos/register.dto';
+
+@Controller('auth')
+export class AuthController {
+  @Post('register')
+  register(@Body() body: RegisterDto) {
+    return body;
+  }
+}
