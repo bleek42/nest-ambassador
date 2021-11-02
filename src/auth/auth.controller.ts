8367d9148c4b5ed70d85ba955b6eeeba12b93865
bleek42 <brandonleek42@gmail.com>
2021-11-02 11:48:19 -0400

    fixed peer deps version issues; auth/admin/login endpoint working with jwt token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,30 +5,49 @@ import {
   Controller,
   NotFoundException,
   Post,
+  Res,
 } from '@nestjs/common';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
+import { Response } from 'express';
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Post('admin/login')
   public async login(
     @Body('username') username: string,
     @Body('password') password: string,
-  ): Promise<void> {
+    @Res({ passthrough: true }) response: Response,
+  ): Promise<{ message }> {
     const user = await this.userService.findByUsername(username);
     console.log(user);
 
     if (!user) {
       throw new NotFoundException(`User with ${username} not found!`);
     }
+
     const comparePasswords = await this.validateHashedPassword(
       password,
       user.password,
     );
+
     if (!comparePasswords) {
       throw new BadRequestException('Incorrect password!');
     }
+
+    const jwt = await this.jwtService.signAsync({
+      id: user.id,
+    });
+
+    response.cookie('jwt', jwt);
+
+    return {
+      message: `User ${user.username} logged in!`,
+    };
   }
 
   private async validateHashedPassword(
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,19 @@ import { UserModule } from './../user/user.module';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    JwtModule.register({
+      secret:
+        '684fd5864a28236520baf8623d3aa0dea4f5c893adcf72880711280da8673e8e312e8c4821e181a4e1e49131f60ed18ebaa730b97debb51e08a4bd0c0e915cfc',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
